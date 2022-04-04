d2990a623ee151a27f4121908c976cebe8009c4f
Afrie Irham <afrieirham.work@gmail.com>
2022-04-05 07:41:46 +0800

    Sort comments in reverse chronological order

--- util/getAllComments.js
+++ util/getAllComments.js
@@ -1,19 +1,23 @@
 import fs from 'fs'
 import path from 'path'
+import { compareDesc, parseISO } from 'date-fns'
 
 export const getAllComments = async () => {
   const guestbookDirectory = path.resolve('.', 'public', 'sign-here')
   const fileNames = fs.readdirSync(guestbookDirectory)
 
-  const commentsPromise = fileNames.map(async (username) => {
-    const commentDirectory = path.resolve(guestbookDirectory, username)
+  const commentsPromise = fileNames.map(async (fileName) => {
+    const commentDirectory = path.resolve(guestbookDirectory, fileName)
     const rawComment = await fs.promises.readFile(commentDirectory, 'utf-8')
 
-    return JSON.parse(rawComment)
+    const username = fileName.split('.')[0]
+
+    return { ...JSON.parse(rawComment), username }
   })
 
   const comments = await Promise.all(commentsPromise)
+  const sortedComments = comments.sort((a, b) => compareDesc(parseISO(a.date), parseISO(b.date)))
 
-  return comments
+  return sortedComments
 }
 
